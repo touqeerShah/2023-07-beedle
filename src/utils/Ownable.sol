3ff4f01ee40c148e03459f1529f51cb5c0e7e0cc
s.touqeer@pharmatrace.io <s.touqeer@pharmatrace.io>
2023-07-31 23:12:04 +0200

    update access modifier and reduce gase

--- src/utils/Ownable.sol
+++ src/utils/Ownable.sol
@@ -2,22 +2,23 @@
 pragma solidity ^0.8.19;
 
 abstract contract Ownable {
-
     event OwnershipTransferred(address indexed user, address indexed newOwner);
 
     address public owner;
 
-    modifier onlyOwner() virtual {
+    modifier onlyOwner() {
         require(msg.sender == owner, "UNAUTHORIZED");
         _;
     }
+
     constructor(address _owner) {
         owner = _owner;
         emit OwnershipTransferred(address(0), _owner);
     }
 
     function transferOwnership(address _owner) public virtual onlyOwner {
+        address oldOwner = _owner;
         owner = _owner;
-        emit OwnershipTransferred(msg.sender, _owner);
+        emit OwnershipTransferred(oldOwner, _owner);
     }
 }
